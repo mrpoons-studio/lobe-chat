cab32f86c9cb6006b63fbfc901e4646c093a2f89
arvinxx <arvinx@foxmail.com>
2024-10-25 21:42:22 +0800

    🚧 wip: add lm studio

---
+++ src/libs/agent-runtime/lmstudio/index.ts
@@ -0,0 +1,10 @@
+import { ModelProvider } from '../types';
+import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
+
+export const LobeLMStudioAI = LobeOpenAICompatibleFactory({
+  baseURL: 'http://localhost:1234/v1',
+  debug: {
+    chatCompletion: () => process.env.DEBUG_LMSTUDIO_CHAT_COMPLETION === '1',
+  },
+  provider: ModelProvider.LMStudio,
+});
